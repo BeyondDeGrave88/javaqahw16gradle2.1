2ef034642cbd6dcac1379f4d2d7d3aa6cefd1bd8
Dmitrij Sokolov <darkfalken88@gmail.com>
2025-09-22 14:18:52 +0300

    OrderTest fixed

--- src/test/java/ru/netology/OrderTest.java
+++ src/test/java/ru/netology/OrderTest.java
@@ -25,7 +25,7 @@ class OrderTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
-        //options.addArguments("--headless");
+        options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.get("http://localhost:9999");
     }
